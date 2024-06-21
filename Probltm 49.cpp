41dfde53d1695cf3ae9a6d48b9d00228a712d2f3
SHAURYA AWASTHI <89693063+Shauryaa007@users.noreply.github.com>
2024-06-21 08:42:08 +0530

    day 49

---
+++ Probltm 49.cpp
@@ -0,0 +1,19 @@
+// 215. Kth Largest Element in an Array
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int>pq;
+        int n=nums.size();
+
+        for(int i=0;i<n;i++)
+        {
+            pq.push(nums[i]);
+        }
+        k--;
+
+        while(k--)pq.pop();
+
+        return pq.top();
+    }
+};
